ff99cf9035975555481693e6e7f3a5b0a041dfd2
Andrey Sugonyaev <34348146+oneir0mancer@users.noreply.github.com>
2024-02-25 00:16:41 +0300

    add option to hide preview

--- ColabUI/Img2ImgRefinerUI.py
+++ ColabUI/Img2ImgRefinerUI.py
@@ -1,6 +1,6 @@
 import torch
 from IPython.display import display
-from ipywidgets import FloatSlider, Dropdown, HBox, VBox, Text, Button
+from ipywidgets import FloatSlider, Dropdown, HBox, VBox, Text, Button, Label
 from PIL import Image
 import ipywidgets as widgets
 import io
@@ -12,18 +12,34 @@ class Img2ImgRefinerUI:
         self.__generator = torch.Generator(device="cuda")
         self.__base_ui = ui
         self.__current_file = ""
+        self.__current_img = ""
+        self.__preview_visible = True
         
         self.strength_field = FloatSlider(value=0.35, min=0, max=1, step=0.05, description="Strength: ")
-        self.upscale_field = FloatSlider(value=1, min=1, max=4, step=0.5, description="Upscale factor: ")
+        self.upscale_field = FloatSlider(value=1, min=1, max=4, step=0.25, description="Upscale factor: ")
         self.path_field = Text(description="Url:", placeholder="Image path...")
         self.load_button = Button(description='Choose')
-        self.image_preview = widgets.Image()
+        self.image_preview = widgets.Image(width=300, height=400)
+        self.hide_button = Button(description='Hide')
+        self.size_label = Label(value="Size")
         
         def load_handler(b):
             if not os.path.isfile(self.path_field.value): return
             self.__current_file = self.path_field.value
             self.image_preview.value = open(self.__current_file, "rb").read()
+            self.__current_img = Image.open(self.__current_file)
+            self.size_label.value = f"{self.__current_img.size} → {self.get_upscaled_size(self.__current_img.size)}"
         self.load_button.on_click(load_handler)
+
+        def hide_handler(b):
+            self.__preview_visible = not self.__preview_visible
+            self.image_preview.layout.width = "300px" if self.__preview_visible else "0px"
+            self.hide_button.description = "Hide" if self.__preview_visible else "Show preview"
+        self.hide_button.on_click(hide_handler)
+
+        def on_slider_change(change):
+            self.size_label.value = f"{self.__current_img.size} → {self.get_upscaled_size(self.__current_img.size)}"
+        self.upscale_field.observe(on_slider_change, 'value')
         
     def generate(self, pipe, init_image=None, generator=None):
         """Generate images given Img2Img Pipeline, and settings set in Base UI and Refiner UI."""
@@ -37,7 +53,7 @@ class Img2ImgRefinerUI:
             init_image = Image.open(self.__current_file)
         
         init_image = init_image.convert('RGB')
-        size = (int(self.upscale_field.value * init_image.size[0]), int(self.upscale_field.value * init_image.size[1]))
+        size = self.get_upscaled_size(init_image.size)
         init_image = init_image.resize(size, resample=Image.LANCZOS)
         
         g = torch.cuda.manual_seed(seed)
@@ -50,6 +66,7 @@ class Img2ImgRefinerUI:
                        num_images_per_prompt = self.__base_ui.batch_field.value,
                        guidance_scale=self.__base_ui.cfg_field.value, 
                        strength=self.strength_field.value,
+                       width=size[0], height=size[1],
                        generator=g)
         return results
 
@@ -57,9 +74,12 @@ class Img2ImgRefinerUI:
     def metadata(self):
         return self._metadata 
 
+    def get_upscaled_size(self, size):
+        return (int(self.upscale_field.value * size[0]), int(self.upscale_field.value * size[1]))
+
     @property
     def render_element(self): 
-        return VBox([self.strength_field, self.upscale_field, HBox([self.path_field, self.load_button]), self.image_preview])
+        return VBox([self.strength_field, self.upscale_field, HBox([self.path_field, self.load_button]), self.size_label, self.image_preview, self.hide_button])
 
     def render(self):
         display(self.render_element)
